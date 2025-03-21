e3801bc32d5b5f22a0189c51c27b632a6a3b2327
nta <NoelBansikah.Tandap@adorsys.com>
2025-03-21 14:00:04 +0100

    presentation

---
+++ script.js
@@ -0,0 +1,27 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide");
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.remove("active");
+        if (i === index) {
+            slide.classList.add("active");
+        }
+    });
+}
+
+function nextSlide() {
+    if (currentSlide < slides.length - 1) {
+        currentSlide++;
+        showSlide(currentSlide);
+    }
+}
+
+function prevSlide() {
+    if (currentSlide > 0) {
+        currentSlide--;
+        showSlide(currentSlide);
+    }
+}
+
+showSlide(currentSlide);
